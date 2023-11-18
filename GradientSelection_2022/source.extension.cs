d74724a58881222b4a903f74516e892a0931ba33
Lokesh Govindu <lokesh.govindu@hexagon.com>
2023-11-18 11:47:45 +0530

    Minor changes

---
+++ GradientSelection_2022/source.extension.cs
@@ -0,0 +1,13 @@
+﻿namespace GradientSelection
+{
+    internal sealed partial class Vsix
+    {
+        public const string Id          = "GradientSelection_2022.cc8c8bcc-11ce-4801-a8d8-bad42f122a9e";
+        public const string Name        = "GradientSelection_2022";
+        public const string Description = @"Make the selection use a gradient brush in rich client experience mode.";
+        public const string Language    = "en-US";
+        public const string Version     = "2023.11.0.0";
+        public const string Author      = "Noah Richards";
+        public const string Tags        = "Editor, Selection";
+    }
+}
